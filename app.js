39d3c3ee847b53daf137fc05c5626a425c05c0a7
Enes <73299223+enescelep@users.noreply.github.com>
2021-07-18 20:28:21 +0300

    Third workshop is done!

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ejs = require('ejs');
+const Post = require('./models/post');
 
 const app = express();
 
+// CONNECT TO DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 //MIDDLEWARE
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -24,6 +38,11 @@ app.get('/add_post', (req, res) => {
 
 app.get('/post', (req, res) => {
   res.render('post');
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
